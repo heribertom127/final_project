678f7031b1d8791ec5a52f412649b891055b5325
heribertom127 <heribertom1270@gmail.com>
2022-04-26 13:37:19 -0700

    fixed the tests

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,9 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
-  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest']
+  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/src/__mocks__/fileMock.js'
+  }
 }
---
+++ src/fileTransformer.js
@@ -0,0 +1,7 @@
+import { basename } from 'path'
+
+export function process(sourceText, sourcePath, options) {
+  return {
+    code: `module.exports = ${JSON.stringify(basename(sourcePath))};`
+  }
+}
