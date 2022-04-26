baa45b804dfb8f8ed99f5be612c769653088a91c
heribertom127 <heribertom1270@gmail.com>
2022-04-26 12:14:00 -0700

    took out the divs

--- src/components/QuoteBox.tsx
+++ src/components/QuoteBox.tsx
@@ -13,31 +13,29 @@ const moveVariants = {
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function QuoteBox({ quote }: { quote: any }) {
   return (
-    <div>
-      <motion.div
+    <motion.div
+      layout
+      className="p-8 w-1/2 bg-[#9CD08E]"
+      variants={moveVariants}
+      initial="hidden"
+      animate="visible"
+    >
+      <motion.p
         layout
-        className="p-8 w-1/2 bg-[#9CD08E]"
-        variants={moveVariants}
-        initial="hidden"
-        animate="visible"
+        className=" font-serif text-[25.89px] text-neutral-900"
+        key={quote.quote}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5 }}
       >
-        <motion.p
-          layout
-          className=" font-serif text-[25.89px] text-neutral-900"
-          key={quote.quote}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.5 }}
-        >
-          &quot;{quote?.quote}&quot;
-        </motion.p>
+        &quot;{quote?.quote}&quot;
+      </motion.p>
 
-        <h2 className="font-serif text-[19px] font-semibold text-neutral-800 ">
-          {' '}
-          - Kanye West
-        </h2>
-      </motion.div>
-    </div>
+      <h2 className="font-serif text-[19px] font-semibold text-neutral-800 ">
+        {' '}
+        - Kanye West
+      </h2>
+    </motion.div>
   )
 }
 
